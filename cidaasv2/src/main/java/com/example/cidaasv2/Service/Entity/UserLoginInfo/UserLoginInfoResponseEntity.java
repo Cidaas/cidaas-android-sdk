6ecbb82aeb84291a41a9b1bd869d1493af112df8
Cidaas <developer@cidaas.de>
2019-03-07 11:18:18 +0530

    Change userinfo to List

--- cidaasv2/src/main/java/com/example/cidaasv2/Service/Entity/UserLoginInfo/UserLoginInfoResponseEntity.java
+++ cidaasv2/src/main/java/com/example/cidaasv2/Service/Entity/UserLoginInfo/UserLoginInfoResponseEntity.java
@@ -1,11 +1,17 @@
 package com.example.cidaasv2.Service.Entity.UserLoginInfo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.io.Serializable;
+import java.util.List;
 
 public class UserLoginInfoResponseEntity implements Serializable {
     private Boolean success;
     private int status;
-    private UserLoginInfoResponseDataEntity[] data;
+   // private UserLoginInfoResponseDataEntity[] data;
+
+    @JsonProperty("data")
+    private List<UserLoginInfoResponseDataEntity> data;
 
 
     public Boolean getSuccess() {
@@ -24,11 +30,19 @@ public class UserLoginInfoResponseEntity implements Serializable {
         this.status = status;
     }
 
-    public UserLoginInfoResponseDataEntity[] getData() {
+    public List<UserLoginInfoResponseDataEntity> getData() {
+        return data;
+    }
+
+    public void setData(List<UserLoginInfoResponseDataEntity> data) {
+        this.data = data;
+    }
+
+    /* public UserLoginInfoResponseDataEntity[] getData() {
         return data;
     }
 
     public void setData(UserLoginInfoResponseDataEntity[] data) {
         this.data = data;
-    }
+    }*/
 }
