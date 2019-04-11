dd6884447971a5540ec4796ff832dd7d67f8ef16
Cidaas <developer@cidaas.de>
2019-04-11 17:46:53 +0530

    Changes in cidaas xml

--- cidaasv2/src/main/java/com/example/cidaasv2/Controller/Repository/ChangePassword/ChangePasswordController.java
+++ cidaasv2/src/main/java/com/example/cidaasv2/Controller/Repository/ChangePassword/ChangePasswordController.java
@@ -25,72 +25,74 @@ public class ChangePasswordController {
 
     public ChangePasswordController(Context contextFromCidaas) {
 
-        verificationType="";
-        context=contextFromCidaas;
-        authenticationType="";
+        verificationType = "";
+        context = contextFromCidaas;
+        authenticationType = "";
         //Todo setValue for authenticationType
 
     }
 
     String codeVerifier, codeChallenge;
+
     // Generate Code Challenge and Code verifier
-    public void generateChallenge(){
+    public void generateChallenge() {
         OAuthChallengeGenerator generator = new OAuthChallengeGenerator();
 
-        codeVerifier=generator.getCodeVerifier();
-        codeChallenge= generator.getCodeChallenge(codeVerifier);
+        codeVerifier = generator.getCodeVerifier();
+        codeChallenge = generator.getCodeChallenge(codeVerifier);
 
     }
 
-    public static ChangePasswordController getShared(Context contextFromCidaas )
-    {
+    public static ChangePasswordController getShared(Context contextFromCidaas) {
         try {
 
             if (shared == null) {
                 shared = new ChangePasswordController(contextFromCidaas);
             }
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             Timber.i(e.getMessage());
         }
         return shared;
     }
 
     //ChangePasswordService
-    public void changePassword(@NonNull final ChangePasswordRequestEntity changePasswordRequestEntity, final Result<ChangePasswordResponseEntity> resetpasswordResult)
-    {
+    public void changePassword(@NonNull final ChangePasswordRequestEntity changePasswordRequestEntity, final Result<ChangePasswordResponseEntity> resetpasswordResult) {
         try {
             CidaasProperties.getShared(context).checkCidaasProperties(new Result<Dictionary<String, String>>() {
                 @Override
                 public void success(Dictionary<String, String> result) {
-                    String baseurl=result.get("DomainURL");
-                    if(changePasswordRequestEntity.getConfirm_password() != null && !changePasswordRequestEntity.getConfirm_password().equals("")
-                            && changePasswordRequestEntity.getNew_password() != null && !changePasswordRequestEntity.getNew_password().equals("")
-                            && changePasswordRequestEntity.getIdentityId() != null && !changePasswordRequestEntity.getIdentityId().equals("")
-                            && changePasswordRequestEntity.getOld_password() != null && !changePasswordRequestEntity.getOld_password().equals("")
-                            && baseurl != null && !baseurl.equals("")){
-
-                        ChangePasswordService.getShared(context).changePassword(changePasswordRequestEntity, baseurl,null, resetpasswordResult);
-                    }
-                    else{
-                        resetpasswordResult.failure(WebAuthError.getShared(context).propertyMissingException("Confirm Password or new password or IdentityID or Old password or baseurl must not be null"));
-                    }
+                    String baseurl = result.get("DomainURL");
+                    checkandChangePasswordService(baseurl, changePasswordRequestEntity, resetpasswordResult);
                 }
 
                 @Override
                 public void failure(WebAuthError error) {
-                   resetpasswordResult.failure(error);
+                    resetpasswordResult.failure(error);
                 }
             });
 
 
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             resetpasswordResult.failure(WebAuthError.getShared(context).serviceException("Exception :ChangePassword Controller :changePassword()",
-                    WebAuthErrorCode.CHANGE_PASSWORD_FAILURE,e.getMessage()));
-            Timber.e(e.getMessage());
+                    WebAuthErrorCode.CHANGE_PASSWORD_FAILURE, e.getMessage()));
+        }
+    }
+
+    private void checkandChangePasswordService(String baseurl, @NonNull final ChangePasswordRequestEntity changePasswordRequestEntity, final Result<ChangePasswordResponseEntity> resetpasswordResult) {
+        try {
+            if (changePasswordRequestEntity.getConfirm_password() != null && !changePasswordRequestEntity.getConfirm_password().equals("")
+                    && changePasswordRequestEntity.getNew_password() != null && !changePasswordRequestEntity.getNew_password().equals("")
+                    && changePasswordRequestEntity.getIdentityId() != null && !changePasswordRequestEntity.getIdentityId().equals("")
+                    && changePasswordRequestEntity.getOld_password() != null && !changePasswordRequestEntity.getOld_password().equals("")
+                    && baseurl != null && !baseurl.equals("")) {
+
+                ChangePasswordService.getShared(context).changePassword(changePasswordRequestEntity, baseurl, null, resetpasswordResult);
+            } else {
+                resetpasswordResult.failure(WebAuthError.getShared(context).propertyMissingException("Confirm Password or new password or IdentityID or Old password or baseurl must not be null"));
+            }
+        } catch (Exception e) {
+            resetpasswordResult.failure(WebAuthError.getShared(context).serviceException("Exception :ChangePassword Controller :checkandChangePasswordService()",
+                    WebAuthErrorCode.CHANGE_PASSWORD_FAILURE, e.getMessage()));
         }
     }
 }
