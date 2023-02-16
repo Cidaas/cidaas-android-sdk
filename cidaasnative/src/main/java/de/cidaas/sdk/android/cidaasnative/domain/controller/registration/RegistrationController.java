bd8422edda981bd97dd2c447ad36eab662a87c04
Alvin Chandra <alvin.chandra@widas.de>
2023-02-16 16:34:42 +0100

    Fix Unprotected Storage of Credentials Vulnerabilties

--- cidaasnative/src/main/java/de/cidaas/sdk/android/cidaasnative/domain/controller/registration/RegistrationController.java
+++ cidaasnative/src/main/java/de/cidaas/sdk/android/cidaasnative/domain/controller/registration/RegistrationController.java
@@ -147,9 +147,13 @@ public class RegistrationController {
                                     checkNotNull(dataEntity, "given_name", registrationEntity.getGiven_name());
                                     checkNotNull(dataEntity, "family_name", registrationEntity.getFamily_name());
                                     checkNotNull(dataEntity, "mobile_number", registrationEntity.getMobile_number());
-                                    checkNotNull(dataEntity, "password", registrationEntity.getPassword());
                                     checkNotNull(dataEntity, "username", registrationEntity.getUsername());
                                     checkNotNull(dataEntity, "birthdate", registrationEntity.getBirthdate().toString());
+                                    if (dataEntity.getFieldKey().equals("password")
+                                            && dataEntity.isRequired()
+                                            && registrationEntity.getPassword().isEmpty()) {
+                                        registerFieldsresult.failure(WebAuthError.getShared(context).propertyMissingException("password must not be empty", methodName));
+                                    }
 
                                     if (dataEntity.getFieldKey().equals("password_echo")) {
                                         if (dataEntity.isRequired() && registrationEntity.getPassword_echo().equals("")) {
