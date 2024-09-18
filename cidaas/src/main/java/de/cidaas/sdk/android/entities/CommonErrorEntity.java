7c37bd7f14706ad1b2834d059cf015b8e0a3025c
widasraja <raja.narayanan@widas.in>
2024-09-18 14:25:40 +0530

    feat([1036](https://gitlab.widas.de/cidaas-v2/verification/issues/-/issues/1036)): update the Android SDK TO Handle both object and string errors

--- cidaas/src/main/java/de/cidaas/sdk/android/entities/CommonErrorEntity.java
+++ cidaas/src/main/java/de/cidaas/sdk/android/entities/CommonErrorEntity.java
@@ -1,16 +1,23 @@
 package de.cidaas.sdk.android.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.Serializable;
+import java.util.LinkedHashMap;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CommonErrorEntity implements Serializable {
 
     private boolean success;
     private int status;
-    private ErrorEntity Error;
-    private String refnumber;
+    private Object error;
+    private Object code;
+
+
+
+    private String referenceNumber;
+
     private String error_description;
 
 
@@ -22,13 +29,7 @@ public class CommonErrorEntity implements Serializable {
         this.error_description = error_description;
     }
 
-    public String getRefnumber() {
-        return refnumber;
-    }
 
-    public void setRefnumber(String refnumber) {
-        this.refnumber = refnumber;
-    }
 
     public boolean isSuccess() {
         return success;
@@ -46,12 +47,45 @@ public class CommonErrorEntity implements Serializable {
         this.status = status;
     }
 
-
-    public ErrorEntity getError() {
-        return Error;
+    public Object getError() {
+        return error;
     }
 
-    public void setError(ErrorEntity error) {
-        Error = error;
+    public void setError(Object error) {
+        this.error = error;
+    }
+
+    public Object getCode() {
+        return code;
+    }
+
+    public void setCode(Object code) {
+        this.code = code;
+    }
+
+    public String getReferenceNumber() {
+        return referenceNumber;
+    }
+
+    public void setReferenceNumber(String referenceNumber) {
+        this.referenceNumber = referenceNumber;
+    }
+
+
+    // Method to check if the error is a string or an object
+    public String getErrorAsString() {
+        if (error instanceof String) {
+            return (String) error;
+        }
+        return null;
+    }
+
+    public ErrorEntity getErrorAsObject() {
+        if (error instanceof LinkedHashMap) {
+            // Deserialize LinkedHashMap into ErrorEntity
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.convertValue(error, ErrorEntity.class);
+        }
+        return null;
     }
 }
